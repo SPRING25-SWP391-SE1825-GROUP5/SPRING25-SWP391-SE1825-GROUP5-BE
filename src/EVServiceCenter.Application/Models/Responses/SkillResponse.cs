9ee576e309d1bda9b331eafe33dea1742b36174a
Nika <tienvmse182865@fpt.edu.vn>
2025-10-01 00:54:22 +0700

    feat/Add Skills CRUD (repo/service/controller), DI wiring; add technician skill upsert/delete admin APIs

---
+++ src/EVServiceCenter.Application/Models/Responses/SkillResponse.cs
@@ -0,0 +1,10 @@
+namespace EVServiceCenter.Application.Models.Responses;
+
+public class SkillResponse
+{
+	public int SkillId { get; set; }
+	public string Name { get; set; }
+	public string? Description { get; set; }
+}
+
+

1740abe2bb987c277a23de303516e3e0d8e82fa0
datnt171874 <danielsmith2511@gmail.com>
2025-10-09 23:54:09 +0700

    Add: Set Vehicle Reminders & Get Vehicle Alert Reminder

---
+++ src/EVServiceCenter.Application/Models/Requests/SetVehicleRemindersRequest.cs
@@ -0,0 +1,22 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace EVServiceCenter.Application.Models.Requests
+{
+    public class SetVehicleRemindersRequest
+    {
+        [Required]
+        public List<SetVehicleReminderItem> Items { get; set; } = new List<SetVehicleReminderItem>();
+    }
+
+    public class SetVehicleReminderItem
+    {
+        [Required]
+        public int ServiceId { get; set; }
+        
+        public DateTime? DueDate { get; set; }
+        
+        public int? DueMileage { get; set; }
+    }
+}
